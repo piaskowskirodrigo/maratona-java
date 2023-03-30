ff16d77fc3d48b3aa725801079f61b2ff1c14e23
piaskowskirodrigo <piaskowskirodrigo@agmail.com>
2023-03-29 21:40:30 -0300

    teste

--- src/logica/ExercicioNumeroPar.java
+++ src/logica/ExercicioNumeroPar.java
@@ -1,12 +1,12 @@
 package logica;
 
-public class ExercicioNumeroPar {
-    public static void main(String[] args) {
-        int numero = 1001;
-        if ( (numero % 2) == 0 ) {
-            System.out.println("Par");
-        } else {
-            System.out.println("IMPAR");
-        }
-    }
-}
+//public class ExercicioNumeroPar {
+    //public static void main(String[] args) {
+       // int numero = 1001;
+//            System.out.println("Par");
+       // } else {
+       //     System.out.println("IMPAR");
+     //   }
+  //
+   // }
+//}
