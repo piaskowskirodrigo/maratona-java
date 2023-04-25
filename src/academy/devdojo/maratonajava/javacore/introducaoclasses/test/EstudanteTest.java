f57a74496fd1c6074a8889b843a7f8dc54d86f62
piaskowskirodrigo <piaskowskirodrigo@agmail.com>
2023-04-25 14:54:34 -0300

    ...

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest2.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest2 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante.nome);
+
+
+        }
+}
