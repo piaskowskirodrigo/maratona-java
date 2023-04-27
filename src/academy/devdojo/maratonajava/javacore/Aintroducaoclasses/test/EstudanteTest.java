a4941c9ea1fd29df5692212c1a204781235f06f6
piaskowskirodrigo <piaskowskirodrigo@gmail.com>
2023-04-27 16:30:27 -0300

    ...

---
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "rodrigo";
+        estudante.idade = 24;
+        estudante.sexo = 'M';
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante);
+
+
+        
+
+    }
+}
--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest2.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest2.java
@@ -1,10 +1,13 @@
-package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
 
-import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
 
-public class EstudanteTest2 {
+public class  EstudanteTest2 {
     public static void main(String[] args) {
         Estudante estudante = new Estudante();
+
+
+        estudante.nome = "";
         System.out.println(estudante.idade);
         System.out.println(estudante.sexo);
         System.out.println(estudante.nome);
